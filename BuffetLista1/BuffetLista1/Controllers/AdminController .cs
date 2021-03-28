0da61ffd0e02477a1c6d1aad6e1faaf44fea0590
PedroHaratani <shunzoh2011@hotmail.com>
2021-03-28 12:48:30 -0300

    commit inicial

---
+++ BuffetLista1/BuffetLista1/Controllers/AdminController .cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BuffetLista1.Models;
+
+namespace BuffetLista1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(ILogger<AdminController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+        public IActionResult Painel() 
+        {
+            return View();
+        }
+        public IActionResult Secao() 
+        {
+            return View();
+        }
+        public IActionResult Ajuda() 
+        {
+            return View();
+        }
+        public IActionResult Termos() 
+        {
+            return View();
+        }
+        public IActionResult Politica() 
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
